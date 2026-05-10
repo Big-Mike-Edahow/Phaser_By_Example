7b676cf49326b49abe369c2770e10baa4ef937e0
Big-Mike-Edahow <perigran_falcon@yahoo.com>
2026-05-10 15:00:58 -0600

    Plays okay.

---
+++ Chapter_03/WallHammer/static/js/main.js
@@ -0,0 +1,28 @@
+// main.js
+
+import Bootloader from "./scenes/bootloader.js";
+import Splash from "./scenes/splash.js";
+import Transition from "./scenes/transition.js";
+import Game from "./scenes/game.js";
+import Outro from "./scenes/outro.js";
+
+const config = {
+    width: 1000,
+    height: 800,
+    scale: {
+        mode: Phaser.Scale.FIT,
+        autoCenter: Phaser.Scale.CENTER_BOTH,
+    },
+    autoRound: false,
+    parent: "game-canvas",
+    physics: {
+        default: "arcade",
+        arcade: {
+            gravity: { y: 300 },
+            debug: true,
+        },
+    },
+    scene: [Bootloader, Splash, Transition, Game, Outro],
+};
+
+const game = new Phaser.Game(config);
